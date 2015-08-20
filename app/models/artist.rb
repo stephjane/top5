d5d8b58bbada2445449462ba5785103f035eb70b
stephjane <stephjanecz@gmail.com>
2015-08-20 14:29:29 -0400

    added paperclip avatar

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,7 @@ class Artist < ActiveRecord::Base
 	has_many :songs
 	validates :name, presence: true
 	validates :name, uniqueness: true
+
+has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+ validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
